384eaa118b550c37fa0a3d6ce94030fc2eeeaf3b
Kremtastic <kristianetakvam@gmail.com>
2024-08-28 11:43:10 +0200

    Upload files

---
+++ demo/src/main/java/com/example/demo/DemoDat250KristianTakvamApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDat250KristianTakvamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDat250KristianTakvamApplication.class, args);
+	}
+
+}
